3f6443a4e13b0100ef5601be2ef82967a34b9ae1
Mariam-Khaz <mariam-khazroun@hotmail.com>
2024-11-20 09:44:29 +0100

    lesson 4

--- third.js
+++ third.js
@@ -1,3 +1,5 @@
+// ----------LESSON 3 --------------------
+
 function validPassword(password, username) {
   if (password.length < 8) {
     return false
@@ -11,3 +13,31 @@ function validPassword(password, username) {
 }
 
 console.log(validPassword("MariamK2024", "MariamK"))
+
+
+// --------- LESSON 4 ---------------
+
+const target = Math.floor (Math.random()*10) +1
+function guessNumberGame () {
+  let guess = null
+  while (guess !== target) {
+  guess = parseFloat (prompt("Guess a number between 1 and 10"))
+    if (guess === target) {
+    alert("You guessed correctly")
+    }
+    else if (guess < target) {
+      alert("You've guessed to low")
+      console.log (`${target}`)
+    }
+    else if (guess > target) {
+      alert("You've guessed to high")
+      console.log (`${target}`)
+    }
+    else {
+      alert ("Error")
+    }
+  }
+
+}
+
+guessNumberGame ()
